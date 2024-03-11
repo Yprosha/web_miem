6d6e8ab926792769016bea758d2dac51fc711ba4
poyakuba1 <prohor.yakuba@gmail.com>
2024-03-11 18:46:12 +0300

    fix 2

--- parsing_kinopoisk/random_movie.py
+++ parsing_kinopoisk/random_movie.py
@@ -5,14 +5,21 @@ TOKEN = "VPRW6KG-FWDMFXR-QZZDK77-6KAZ6TP"
 kp = KinopoiskDev(token=TOKEN)
 
 def get_random_film():
-    item = kp.random()
-    movie = json.loads(item.json())
+    try:
+        item = kp.random()
+        movie = json.loads(item.json())
 
-    random_mov = {
-        'url': f"https://www.kinopoisk.ru/film/{movie['id']}/",
-        'name': movie['name'],
-        'poster': movie['poster']['url']
-    }
+        random_mov = {
+            'url': f"https://www.kinopoisk.ru/film/{movie['id']}/",
+            'name': movie['name'],
+            'poster': movie['poster']['url']
+        }
+    except:
+        random_mov = {
+                    'url': f"https://www.kinopoisk.ru/film/{41519}/",
+                    'name': 'Брат',
+                    'poster': 'https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/18aa74db-68a3-4dda-a216-2ada3ee1e388/3840x'
+        }
     
     return json.dumps(random_mov)
 
